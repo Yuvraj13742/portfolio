bcfb804b303cf3742c8effdc84c82e0ca39bdfc4
yuvraj13742 <13742shyuvraj@gmail.com>
2026-01-18 17:28:39 +0530

    some changes in resume

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projectsData = [
         title: "Energy Revenue Management System (ERMS)",
         description: "A production-level web application developed during an internship at CedLabs, aimed at digitizing and managing electricity revenue operations. The system focuses on security, scalability, and reliable data handling for real-world usage.",
         tech: ["React", "Node.js", "Express.js", "AWS EC2", "Amazon RDS", "NGINX"],
-        image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800", // Placeholder
+        image: "Screenshot 2026-01-18 142116.png", // Placeholder
         links: {
             demo: "https://erms-mescom.cedlabs.in/",
             github: "#"
@@ -15,7 +15,7 @@ export const projectsData = [
         title: "Pathfinder Participant Manager Website",
         description: "A full-stack event management platform built for marathon and large-scale events. It automates participant registration, finish-line verification using QR scanning, and instant certificate generation with real-time processing.",
         tech: ["Next.js", "Honu", "React Query", "Drizzle ORM", "PDF-lib", "Vercel"],
-        image: "https://images.unsplash.com/photo-1535581652167-3d6b98c36cd0?auto=format&fit=crop&q=80&w=800",
+        image: "Screenshot 2026-01-18 142128.png",
         links: {
             demo: "https://marathon-llsu.vercel.app/#",
             github: "https://github.com/Yuvraj13742/marathon#"
@@ -26,7 +26,7 @@ export const projectsData = [
         title: "HealthVault – Student Healthcare Management System",
         description: "A secure healthcare management system designed for students to manage medical records, appointments, and certificates. The platform includes AI-assisted diagnosis, chatbot support, real-time notifications, and PDF-based medical documentation.",
         tech: ["React", "Node.js", "Express.js", "Flask", "PostgreSQL"],
-        image: "https://images.unsplash.com/photo-1504711434969-e33886168f5c?auto=format&fit=crop&q=80&w=800",
+        image: "Screenshot 2026-01-18 142052.png",
         links: {
             demo: "https://health-vault-git-main-13742shyuvraj-gmailcoms-projects.vercel.app/#",
             github: "https://github.com/Yuvraj13742/health_vault#"
